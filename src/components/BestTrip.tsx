3cb773ed6ad78cbcc95e7574f41aa2d6aedf1ca0
Lê Văn Anh <109362253+levananh202ht@users.noreply.github.com>
2024-04-13 00:06:40 +0700

    Revert "update responsive home"

--- src/components/BestTrip.tsx
+++ src/components/BestTrip.tsx
@@ -21,27 +21,24 @@ const BestTrip = () => {
   const trips = queryGetBestTrip?.data?.data || [];
   return (
     <div className='mx-auto mb-20 max-w-7xl'>
-      <div className='flex flex-col items-center justify-between w-full mb-10 md:flex-row'>
-        <div className='flex flex-col mx-5 space-y-2 md:mx-0 md:mr-5 md:mb-0'>
+      <div className='flex items-center justify-between w-full mb-10'>
+        <div className='flex flex-col space-y-2'>
           <h1 className='text-3xl font-bold'>Plan your best trip ever</h1>
           <p>Making the Most of Your Travel Experience in 2024</p>
         </div>
-        <div className='mx-5 mb-5 md:mb-0'>
-          <Link to='/best-trip'>
-            <Button
-              variant='ghost'
-              className='text-blue-500 border border-blue-500 rounded-full hover:bg-white hover:text-blue-500 hover:shadow-md'
-            >
-              View All Destination
-            </Button>
-          </Link>
-        </div>
+        <Link to='/best-trip'>
+          <Button
+            variant='ghost'
+            className='text-blue-500 border border-blue-500 rounded-full hover:bg-white hover:text-blue-500 hover:shadow-md'
+          >
+            View All Destination
+          </Button>
+        </Link>
       </div>
-
-      <div className='flex flex-wrap items-center justify-center w-full gap-14'>
+      <div className='flex flex-wrap items-center w-full gap-14'>
         {trips.map((trip: TripType) => (
           <div
-            className='flex flex-col rounded-t-3xl rounded-b-lg w-96 h-[20rem] space-y-4 border hover:scale-105 transition-all '
+            className='flex flex-col rounded-t-3xl rounded-b-lg w-96 h-[20rem] space-y-4 border hover:scale-105 transition-all'
             key={trip.id}
           >
             <img
--- src/pages/banner/Banner.tsx
+++ src/pages/banner/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
         <CarouselPlugin></CarouselPlugin>
       </div>
       <div className='relative'>
-        <h1 className='absolute inset-0 flex items-center justify-center text-white z-1 top-[16rem] text-7xl font-bold text-center left-[7rem] leading-tight md:block hidden'>
+        <h1 className='absolute inset-0 flex items-center justify-center text-white z-1 top-[16rem] text-7xl font-bold text-center left-[7rem] leading-tight'>
           Discover New Places and Create <br /> Unforgettable Memories
         </h1>
       </div>
