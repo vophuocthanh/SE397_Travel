587642d9e7228842f692c1a340aa4749f2a9ee2f
Võ Phước Thạnh <92651849+vophuocthanh@users.noreply.github.com>
2024-04-14 23:59:08 +0700

    fix(UI): fix style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ import Footer from './footer/Footer';
 const Home = () => {
   return (
     <div className='relative r-0'>
-      <Header className='flex items-center justify-between w-full px-10 py-4 mx-auto bg-transparent' />
+      <Header className='flex items-center justify-between w-full px-6 py-4 mx-auto bg-transparent' />
       <Banner />
       <FeaturedPage />
       <BestTrip />
